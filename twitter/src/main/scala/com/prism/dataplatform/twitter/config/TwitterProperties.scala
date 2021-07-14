d11460c1c512beae3f3e62609a7d1cdb673c85bb
Denis Yerusalimtsev <denis.yerusalimtsev@gmail.com>
2021-07-14 15:59:41 +0300

    Add gradle to gitignore

--- twitter/src/main/scala/com/prism/dataplatform/twitter/config/TwitterProperties.scala
+++ twitter/src/main/scala/com/prism/dataplatform/twitter/config/TwitterProperties.scala
@@ -6,7 +6,7 @@ import org.apache.flink.api.common.typeinfo.TypeInformation
 import java.util.Properties
 
 trait TwitterProperties {
-  implicit val evidence = TypeInformation.of(classOf[String])
+  implicit val evidence: TypeInformation[String] = TypeInformation.of(classOf[String])
 
   def setupProperties(config: TwitterConfig): Properties = {
     val properties = new Properties
