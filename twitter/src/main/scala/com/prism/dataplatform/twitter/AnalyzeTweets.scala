ddcf12fe17a9e764b6523565d6a7229a2e04ed47
Denis Yerusalimtsev <denis.yerusalimtsev@gmail.com>
2021-09-05 17:50:50 +0300

    Refactor code, add unit testing, add Rules Processor

--- twitter/src/main/scala/com/prism/dataplatform/twitter/AnalyzeTweets.scala
+++ twitter/src/main/scala/com/prism/dataplatform/twitter/AnalyzeTweets.scala
@@ -4,14 +4,26 @@ import cats.effect.unsafe.implicits.global
 import com.prism.dataplatform.flink.FlinkJob
 import com.prism.dataplatform.twitter.client.TwitterRestClient
 import com.prism.dataplatform.twitter.config.Config
+import com.prism.dataplatform.twitter.entities.Rule
+import com.prism.dataplatform.twitter.entities.requests.AddRules
+import com.prism.dataplatform.twitter.processor.RulesProcessor
 
 final class AnalyzeTweets extends FlinkJob[Config]
-  with TwitterRestClient {
+  with TwitterRestClient
+  with RulesProcessor {
   override def script(): Unit = {
     logger.info("Application started")
     setupSettings(config.twitter)
-    val tweets = authenticate().flatMap(token => searchTweets("lakers", token.access_token)).unsafeRunSync()
-    print(tweets)
+
+    val rules: AddRules = AddRules(Seq[Rule](addRules("spacex")))
+    val program = for {
+      token <- authenticate
+      rules <- applyRules(token.access_token, rules)
+      tweets <- filteredStream(token.access_token)
+    } yield tweets
+
+    program.unsafeRunSync()
+    print(program)
   }
 }
 
