4a02e020fdaa427f339bb53bf46778ed5aa74731
Denis Yerusalimtsev <denis.yerusalimtsev@gmail.com>
2022-01-09 22:10:50 +0200

    Resolve issue with stackoverflow exception by commenting other fields.

--- twitter/src/main/scala/com/prism/dataplatform/twitter/entities/User.scala
+++ twitter/src/main/scala/com/prism/dataplatform/twitter/entities/User.scala
@@ -6,17 +6,17 @@ final case class User(
                        id: String,
                        name: String,
                        username: String,
-                       created_at: Option[Instant],
-                       `protected`: Option[Boolean],
-                       withheld: Option[Withheld],
-                       location: Option[String],
-                       url: Option[String],
-                       description: Option[String],
+//                       created_at: Option[Instant],
+//                       `protected`: Option[Boolean],
+//                       withheld: Option[Withheld],
+//                       location: Option[String],
+//                       url: Option[String],
+//                       description: Option[String],
                        verified: Option[Boolean],
-                       entities: Option[UserEntities],
-                       profile_image_url: Option[String],
+//                       entities: Option[UserEntities],
+//                       profile_image_url: Option[String],
                        public_metrics: Option[UserPublicMetrics],
-                       pinned_tweet_id: Option[String]
+//                       pinned_tweet_id: Option[String]
                      )
 
 final case class UserPublicMetrics(
